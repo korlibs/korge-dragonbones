4be2784aff671c138e170837f1bb93b76427787b
soywiz <soywiz@gmail.com>
2023-03-30 01:49:32 +0200

    Small fix

--- korge-dragonbones/src/commonTest/kotlin/SkipIOTest.kt
+++ korge-dragonbones/src/commonTest/kotlin/SkipIOTest.kt
@@ -1,5 +1,5 @@
-import korlibs.io.util.*
+import korlibs.memory.Platform
 
 //val skipIOTest get() = OS.isJsBrowser
-val skipIOTest get() = OS.isJs || OS.isAndroid
+val skipIOTest get() = Platform.isJs || Platform.isAndroid
 val doIOTest get() = !skipIOTest
