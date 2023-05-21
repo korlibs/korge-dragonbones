a022f5874b9e8aebad52fa1e8a1f80cc6add0b5f
soywiz <soywiz@gmail.com>
2023-05-22 01:33:15 +0200

    Moved example to root + updated to KorGE 4.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import korlibs.korge.gradle.*
 
 plugins {
     //alias(libs.plugins.korge)
-    id("com.soywiz.korge") version "4.0.0-rc4"
+    id("com.soywiz.korge") version "4.0.0"
     //id("com.soywiz.korge") version "999.0.0.999"
 }
 
